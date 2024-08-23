0c31269f304198868354d0588718ce97e961210e
Aniket Raj <aniketraj0224@gmail.com>
2024-08-24 01:11:39 +0530

    Delete Problem08.py

--- Problem08.py
+++
@@ -1,9 +0,0 @@
-#WAP to enter your name and print after deleting first and last characters of the name.
-
-name = input("Enter the name: ")
-
-if len(name)>2:
-    new_name= name[1:-1]
-    print(f"Name after deleting the first and last characters: {new_name}")
-else:
-    print("Name is too short to remove the first and last Characters.")
