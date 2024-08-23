64b0ca2f21914e134a941dcc19ef66e715cc2a5a
Aniket Raj <aniketraj0224@gmail.com>
2024-08-24 01:11:18 +0530

    Delete Problem05b2.py

--- Problem05b2.py
+++
@@ -1,9 +0,0 @@
-# B) WAP to swap string values of two variables
-# 	ii) Without using third variable
-
-a = (input("Enter the first string : ")) #first string 
-b = (input("Enter the second string : ")) #Second string 
-
-a, b = b, a
-
-print(f"The first string is {a} and the second string is {b}.")
