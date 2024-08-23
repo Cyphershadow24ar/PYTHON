d2fccb0f94a76750e2a58778148f05907e911e80
Aniket Raj <aniketraj0224@gmail.com>
2024-08-24 00:23:13 +0530

    Delete Problem01.py

--- Problem01.py
+++
@@ -1,7 +0,0 @@
-# WAP to convert temperature in degree Celsius to Fahrenheit.
-
-c = int(input("Enter the Temperature in Celsius(°C): "))
-
-f = ((9/5) * c) + 32
-
-print(f"{c}°C Celsius is equal to {f}°F Fahrenheit.")
