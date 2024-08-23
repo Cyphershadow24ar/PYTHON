cd362c408f3b9851a7fc9b164e8ba56de43e9620
Aniket Raj <aniketraj0224@gmail.com>
2024-08-24 00:23:49 +0530

    Delete Problem02.py

--- Problem02.py
+++
@@ -1,9 +0,0 @@
-# WAP to input principal amount, rate of interest and time then calculate simple interest and cumulative interest.
-
-PA = int(input("Enter the Principal Amount: ₹"))
-RI = float(input("Enter the Rate of interest(%): "))
-T = int(input("Enter the Time: "))
-
-SI = ((PA * RI * T)/100)
-print(f"The Simple Interest earned over {T} years would be ₹{SI}.")
-
