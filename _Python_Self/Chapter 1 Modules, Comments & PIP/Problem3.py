995740d8d0c633824549f439ac535822c5843a68
Aniket Raj <aniketraj0224@gmail.com>
2024-10-15 14:39:25 +0530

    Add files via upload

---
+++ _Python_Self/Chapter 1 Modules, Comments & PIP/Problem3.py
@@ -0,0 +1,7 @@
+# Install an external module and use it to perform an operation of your interest
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("fuckoff Messi is the Greatest of all time and Fc barcelona is best  .")
+
+engine.runAndWait()
