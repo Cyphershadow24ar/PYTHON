21126fcf017753651bac42bd7731b6326e64ca19
Aniket Raj <aniketraj0224@gmail.com>
2024-10-15 14:47:04 +0530

    Add files via upload

---
+++ Chapter 1 Modules, Comments & PIP/Problem2.py
@@ -0,0 +1,29 @@
+# Use REPL and print the table of 5 using it.
+
+# SyntaxError: invalid syntax
+# >>> 5*1
+# 5
+# >>> 5*2
+# 10
+# >>> 5*#
+#   File "<stdin>", line 1
+#     5*#
+#       ^
+# SyntaxError: invalid syntax
+# >>> 5*3
+# 15
+# >>> 5*4
+# 20
+# >>> 5*5
+# 25
+# >>> 5*6
+# 30
+# >>> 5*7
+# 35
+# >>> 5*8
+# 40
+# >>> 5*9
+# 45
+# >>> 5*10
+# 50
+# >>>
