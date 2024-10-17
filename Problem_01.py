3a0c8cb486ee4a95792528441922c15e4d804a63
Aniket Raj <aniketraj0224@gmail.com>
2024-10-17 08:34:30 +0530

    Delete Problem_01.py

--- Problem_01.py
+++
@@ -1,9 +0,0 @@
-#WAP in python to add two numbers using module.
-
-import mymath
-
-num1 = int(input("Enter the 1st number: "))
-num2 = int(input("Enter the 2st number: "))
-
-sum = mymath.add_numbers(num1,num2)
-print(f"The Sum is: {sum}")
