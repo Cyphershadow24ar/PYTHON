59342f5da97eb2a0d03ed80b5f6210ed43d6d7bb
Aniket Raj <aniketraj0224@gmail.com>
2024-11-03 02:04:11 +0530

    Delete Chapter 9 File I&O directory

--- Chapter 9 File I&O/02_file_write.py
+++
@@ -1,8 +0,0 @@
-
-st = "Hey aniket don't give up your time is now."
-
-f = open("myfile.txt", "a") # here 'w' stands for write.
-
-f.write(st)
-
-f.close()
--- Chapter 9 File I&O/pratice_set/10_problem10.py
+++
@@ -1,7 +0,0 @@
-#Write a program to wipe out the content of a 
-# file using python.
-
-with open("this_copy.txt", "w") as f:
-    f.write("") 
-
-#Just empty or blank the f.write function.
